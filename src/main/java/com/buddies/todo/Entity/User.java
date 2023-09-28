dbd6ad44e36ef596e0f6c5f9411882192348ed5e
Nithin-ELS <n.kumar4@elsevier.com>
2023-09-28 19:34:36 +0530

    Added User Entity

--- src/main/java/com/buddies/todo/Entity/User.java
+++ src/main/java/com/buddies/todo/Entity/User.java
@@ -1,8 +1,22 @@
 package com.buddies.todo.Entity;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "Users")
-public class User {
+import lombok.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "Users")
+ public class User {
+ 
+	@Id
+	private ObjectId id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	
 }
